cbb5e3e9f56a15461e6ff37eacd65e4a8a8557ff
DESKTOP-8BKARR2\hongjunlee <hong3883@naver.com>
2026-07-20 23:58:57 +0900

    feat: init project for swiw

---
+++ swiw/include/swiw.h
@@ -0,0 +1,13 @@
+#pragma once
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+	void SWIW_GetVersion();
+
+#ifdef __cplusplus
+}
+#endif
---
+++ swiw/src/base/api/swiw.cpp
@@ -0,0 +1,15 @@
+#include "include/swiw.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+	void SWIW_GetVersion()
+	{
+
+	}
+
+#ifdef __cplusplus
+}
+#endif
