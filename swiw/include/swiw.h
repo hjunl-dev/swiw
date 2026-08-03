761187a2b580dc929a52a2593c079ea600354de8
DESKTOP-8BKARR2\hongjunlee <hong3883@naver.com>
2026-08-03 23:56:11 +0900

    feat: add new api

--- swiw/include/swiw.h
+++ swiw/include/swiw.h
@@ -6,6 +6,12 @@ extern "C"
 {
 #endif
 
+	void SWIW_Init();
+
+	void SWIW_Shutdown();
+
+	void SWIW_Create();
+
 	void SWIW_GetVersion(wchar_t* outVersion);
 
 #ifdef __cplusplus
--- swiw/src/base/api/swiw.cpp
+++ swiw/src/base/api/swiw.cpp
@@ -10,6 +10,21 @@ extern "C"
 {
 #endif
 
+    void SWIW_Init()
+    {
+
+    }
+
+    void SWIW_Shutdown()
+    {
+
+    }
+
+    void SWIW_Create()
+    {
+
+    }
+
     void SWIW_GetVersion(wchar_t* outVersion)
     {
         if (!outVersion) return;
