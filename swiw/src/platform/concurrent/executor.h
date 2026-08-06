7607dcea026f3225f94ff6901b56bd5102f4a4aa
DESKTOP-8BKARR2\hongjunlee <hong3883@naver.com>
2026-08-06 23:13:34 +0900

    Concurrent: Added a basic skeleton for the Executor implementation.

--- swiw/src/platform/concurrent/executor.h
+++ swiw/src/platform/concurrent/executor.h
@@ -1,9 +1,51 @@
 #pragma once
+#include <string>
+#include <functional>
+#include <exception>
 
 
-
-
+/**
+* @brief Executor interface for task execution.
+*/
 namespace swiw::platform::concurrent
 {
+	using Job = std::function<void()>;
 
-} // namespace swiw::platform::concurrent
+	class ExecutorException : public std::exception
+	{
+	public:
+		explicit ExecutorException(const char* message) : std::exception(message) {}
+		explicit ExecutorException(const std::string& message) : std::exception(message.c_str()) {}
+	};
+
+	class WorkerException : public std::exception
+	{
+	public:
+		explicit WorkerException(const char* message) : std::exception(message) {}
+		explicit WorkerException(const std::string& message) : std::exception(message.c_str()) {}
+	};
+
+	class Executor
+	{
+	public:
+		Executor() = default;
+		virtual ~Executor() = default;
+
+	public:
+		virtual void Submit(Job&& job) = 0;
+		virtual void Shutdown() = 0;
+		virtual std::vector<Job> ShutdownNow() = 0;
+		virtual bool IsShutdown() const = 0;
+	};
+
+	class Worker
+	{
+	public:
+		Worker() = default;
+		virtual ~Worker() = default;
+
+	public:
+		virtual void Submit(std::function<void()> task) = 0;
+		virtual void Shutdown() = 0;
+	};
+}
--- swiw/src/platform/concurrent/executor/thread_pool.h
+++ swiw/src/platform/concurrent/executor/thread_pool.h
@@ -1,9 +1,38 @@
 #pragma once
+#include <src/platform/concurrent/executor.h>
+#include <src/platform/concurrent/bq.h>
 
-
+#include <memory>
+#include <vector>
+#include <atomic>
 
 
 namespace swiw::platform::concurrent
 {
+	namespace detail
+	{
+		struct ThreadPoolState
+		{
+			std::unique_ptr<BQ<Job>> job_queue_;
+			std::atomic_bool accepting_;
+			std::atomic_bool running_;
+		};
+	}
 
-} // namespace swiw::platform::concurrent
+	class ThreadPool : public Executor
+	{
+	public:
+		ThreadPool(size_t worker_count);
+		~ThreadPool() override;
+
+	public:
+		void Submit(Job&& job) override {}
+		void Shutdown() override {}
+		std::vector<Job> ShutdownNow() override {}
+		bool IsShutdown() const override {}
+
+	private:
+		std::shared_ptr<detail::ThreadPoolState> state_;
+		std::vector<std::unique_ptr<Worker>> workers_;
+	};
+}
