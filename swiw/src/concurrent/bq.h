c57b1d7d0b1b43c639f1c3c0a21c11064d051d2f
DESKTOP-8BKARR2\hongjunlee <hong3883@naver.com>
2026-08-13 23:58:51 +0900

    Concurrent: Add CaddedPadded in bq.h

--- swiw/src/concurrent/bq.h
+++ swiw/src/concurrent/bq.h
@@ -10,6 +10,32 @@
 */
 namespace swiw::concurrent
 {
+	inline constexpr std::size_t kCacheLineAlign = 128;
+
+	template <class T>
+	class alignas(kCacheLineAlign) CachePadded {
+	public:
+		constexpr CachePadded() noexcept(std::is_nothrow_default_constructible_v<T>) = default;
+
+		constexpr explicit CachePadded(T value) noexcept(std::is_nothrow_move_constructible_v<T>)
+			: value_(std::move(value)) {
+		}
+
+		[[nodiscard]] constexpr const T& operator*() const noexcept { return value_; }
+		[[nodiscard]] constexpr const T* operator->() const noexcept { return &value_; }
+		[[nodiscard]] constexpr T& operator*() noexcept { return value_; }
+		[[nodiscard]] constexpr T* operator->() noexcept { return &value_; }
+
+		[[nodiscard]] constexpr const T& get() const noexcept { return value_; }
+		[[nodiscard]] constexpr T& get() noexcept { return value_; }
+
+	private:
+		T value_{};
+	};
+
+	static_assert(sizeof(CachePadded<std::atomic<std::size_t>>) == kCacheLineAlign,
+		"alignas 는 sizeof 도 alignof 의 배수로 올림합니다(= Rust repr(align) 과 동일).");
+
 	template<typename T>
 	class BQ
 	{
