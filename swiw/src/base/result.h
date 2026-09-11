ab443e1be1cb44bf18347599d1ba650acecc6566
DESKTOP-8BKARR2\hongjunlee <hong3883@naver.com>
2026-09-11 23:57:32 +0900

    misc: Move files requiring structural changes to `tmp`, and begin implementation design starting with `result.h`.

---
+++ swiw/src/base/result.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+#include <variant>
+
+namespace swiw::base {
+
+// ============================================================
+//  Err
+// ============================================================
+template <typename E>
+struct Err {
+  E err;
+};
+
+// ============================================================
+//  Result
+// ============================================================
+
+template <typename T, typename E>
+class [[nodiscard]] Result {
+ public:
+  Result(T value)
+      : v_(std::in_place_index<0>, std::move(value)) {}  // Ok(value)
+
+  template <typename TErr>
+    requires std::constructible_from<E, TErr>
+  Result(Err<TErr> e)
+      : v_(std::in_place_index<1>, std::move(e.error)) {}  // Err(e)
+
+  bool IsOk() const noexcept { return v_.index == 0; }
+  bool IsErr() const noexcept { return v_.index == 1; }
+  explicit operator bool() noexcept { return IsOk(); }
+
+  T& Unwrap() & { return std::get<0>(v_); }
+  T&& Unwrap() && { return std::get<0>(std::move(v_)); }
+  E& Error() & { return std::get<1>(v_); }
+  T&& Error() && { return std::get<1>(std::move(v_)); }
+
+ private:
+  std::variant<T, E> v_;
+};
+
+}  // namespace swiw::base
--- swiw/src/tmp/concurrent.h
+++ swiw/src/tmp/concurrent.h
@@ -11,18 +11,43 @@
 #include <type_traits>
 
 namespace swiw::concurrent {
+
 // ============================================================
 // Errors
 // ============================================================
 
 enum class PushError : uint8_t { Ok, Disposed, WouldBlock };
+
+inline const wchar_t* ErrorToString(PushError e) noexcept {
+  switch (e) {
+    case PushError::Ok:
+      return L"Ok";
+    case PushError::Disposed:
+      return L"Queue is disposed";
+    case PushError::WouldBlock:
+      return L"Queue is full";
+  }
+}
+
 enum class PopError : uint8_t { Ok, Disposed, WouldBlock };
+
+inline const wchar_t* ErrorToString(PopError e) noexcept {
+  switch (e) {
+    case PopError::Ok:
+      return L"Ok";
+    case PopError::Disposed:
+      return L"Queue is disposed";
+    case PopError::WouldBlock:
+      return L"Queue is empty";
+  }
+}
 
 // ============================================================
 // Cache-line padding to prevent false sharing
 // rust ver:
 //  #[repr(align(128))] struct CachePadded<T>(T)
-//      impl<T> Deref / DerefMut for CachePadded<T>
+//  impl<T> Deref for CachePadded<T>
+//  impl<T> DerefMut for CachePadded<T>
 // ============================================================
 
 template <typename T>
@@ -39,5 +64,17 @@ struct alignas(128) CachePadded {
   const T& operator*() const noexcept { return value; }
   T* operator->() noexcept { return &value; }
   const T * opeator->() noexcept { return &value; }
+};
+
+template <typename T>
+class BQ {
+ public:
+  virtual ~BQ() = default;
+
+  virtual PushError Push(T& item) = 0;
+  virtual PushError TryPush(T& item) = 0;
+
+  virtual PopError Pop(T& out_item) = 0;
+  virtual PopError TryPop(T& out_item) = 0;
 };
 }  // namespace swiw::concurrent
